eb4e93ab5152ed86e37948c76a19e43066c3309a
Chase <dai12@tju.edu.cn>
2019-03-06 19:13:54 +0800

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class HelloWorld
+    {
+        public static void helloWorld()
+        {
+            Console.WriteLine("HelloWorld!!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld.helloWorld();
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg[{0}]=[{1}]", i, args[i]);
+            }
+        }
+        
+    }
+}
